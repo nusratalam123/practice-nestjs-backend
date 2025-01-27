51cceb48769daffe068ed1948f5ebe1f6230fee0
nusratalam123 <nusratalam975@gmail.com>
2025-01-27 23:57:52 +0600

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientService } from './prisma-client/prisma-client.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const prismaService = app.get(PrismaClientService);
+  prismaService.enableShutdownHooks(app);
+ // Define the port number
+ const port = process.env.PORT || 3000; // Use environment variable or default to 3000
+
+ await app.listen(port);
+
+ // Log the running port
+ console.log(`Backend is running on http://localhost:${port}`);
 }
 bootstrap();
