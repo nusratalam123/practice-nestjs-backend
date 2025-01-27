d73fb517c951b04b0361bbb9bc45e9dd30d0f900
nusratalam123 <nusratalam975@gmail.com>
2025-01-27 23:58:07 +0600

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { PrismaClientService } from './prisma-client/prisma-client.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule],
+  controllers: [],
+  providers: [PrismaClientService],
 })
 export class AppModule {}
