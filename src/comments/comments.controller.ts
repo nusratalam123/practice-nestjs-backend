04b86b96c2af43a3a5e61f1715bf23b97a52b60f
nusratalam123 <nusratalam975@gmail.com>
2025-01-30 00:10:39 +0600

    update

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Post, Body,  Get, UseGuards } from '@nestjs/common';
 import { CommentService } from './comments.service';
 import { Prisma } from '@prisma/client';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
 
 @Controller('comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('create')
   async createComment(@Body() data: Prisma.CommentCreateInput) {
     return this.commentService.createComment(data);
