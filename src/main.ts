395193477631fae88ff67dd03f859afd11371fe8
nusratalam123 <nusratalam975@gmail.com>
2025-01-29 23:31:56 +0600

    login and registration dto add

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientService } from './prisma-client/prisma-client.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaClientService);
   prismaService.enableShutdownHooks(app);
- // Define the port number
- const port = process.env.PORT || 3000; // Use environment variable or default to 3000
 
- await app.listen(port);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    })
+  );
 
- // Log the running port
- console.log(`Backend is running on http://localhost:${port}`);
+  // // ✅ Set Global Prefix to Avoid Unexpected HTML Pages
+  // app.setGlobalPrefix('api'); // Example: All routes will now be /api/...
+
+
+  // Define the port number
+  const port = process.env.PORT || 3000; // Use environment variable or default to 3000
+
+  await app.listen(port);
+
+  // Log the running port
+  console.log(`Backend is running on http://localhost:${port}`);
 }
 bootstrap();
