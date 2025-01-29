62f44a411b9ac46024ac45eb9b23e4bd4052ceac
nusratalam123 <nusratalam975@gmail.com>
2025-01-30 00:15:11 +0600

    update user model

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { PrismaClientModule } from 'src/prisma-client/prisma-client.module';
+import { PrismaClientModule } from '../prisma-client/prisma-client.module';
 
 @Module({
   imports: [PrismaClientModule],
