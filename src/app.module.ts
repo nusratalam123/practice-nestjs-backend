c50242c16aef32d7776fd55b1e505cc15947d7bc
nusratalam123 <nusratalam975@gmail.com>
2025-01-29 23:31:18 +0600

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { PrismaClientService } from './prisma-client/prisma-client.service';
+import { CommentsModule } from './comments/comments.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
+import { ResturantModule } from './resturant/resturant.module';
+
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, CommentsModule, UserModule, AuthModule, ProjectModule, ResturantModule],
   controllers: [],
   providers: [PrismaClientService],
 })
